b94e6ffacd3344f02229dd2a4e3592541b4a6147
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-09-05 13:13:22 -0700

    halfway done with config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,99 +1,10 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Brian Nguyen"
       user-mail-address "brian.edison.nguyen@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
 (setq doom-font (font-spec :family "JetBrainsMonoMedium Nerd Font" :size 15))
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq doom-theme 'doom-one)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-;;(setq display-line-numbers-type t)
-(setq display-line-numbers-type 'relative)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
-;; Configuration
-;;(setq org-latex-listings 'minted)
-;; set transparency
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
-;;(add-to-list 'default-frame-alist '(alpha 85 85))
 
-;; Beacon
-(beacon-mode 1)
-(setq beacon-blink-when-point-moves-vertically 1)
-(setq beacon-blink-when-buffer-changes t)
-(setq beacon-blink-when-window-scrolls t)
-
-;; Org Journal
-(setq org-journal-dir "~/Documents/journal/"
-      org-journal-file-header "#+TITLE: %M %Y"
-      org-journal-date-format "%A, %Y-%m-%d"
-      org-journal-file-type 'monthly
-      org-journal-file-format "%Y-%m.org")
-(setq org-fold-core-style 'overlays)
-
-;; Org Agenda
-(after! org
-  (setq org-agenda-files '("~/Documents/journal/agenda.org"))
-)
-
-(use-package! org-auto-tangle
-  :defer t
-  :hook (org-mode . org-auto-tangle-mode)
-  :config
-  (setq org-auto-tangle-default t))
-
-
-(require 'neotree)
-(global-set-key [f6] 'org-bullets-mode)
-(global-set-key [f7] 'org-export-dispatch)
-(global-set-key [f8] 'neotree-toggle)
-(global-set-key [f9] 'neotree-refresh)
-(global-set-key (kbd "C-c c") '=calendar)
-(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(setq display-line-numbers-type 'relative)
