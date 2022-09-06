91a445ab987f435584c679afc2e000231b89f9ce
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-09-06 12:57:20 -0700

    bspwm autostart nitrogen

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,14 +7,6 @@ function run {
   fi
 }
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-#autorandr horizontal
-
-
 #change your keyboard if you need it
 #setxkbmap -layout be
 
@@ -23,10 +15,8 @@ xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 conky -c $HOME/.config/bspwm/system-overview &
-run variety &
 run nm-applet &
 run pamac-tray &
-run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
 picom --experimental-backends --config $HOME/.config/picom/picom.conf &
@@ -34,18 +24,11 @@ picom --experimental-backends --config $HOME/.config/picom/picom.conf &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
 
-#Some ways to set your wallpaper besides variety or nitrogen
-#feh --bg-scale ~/.config/bspwm/wall.png &
-#feh --bg-fill /home/buraiyen/Pictures/Photography/BEN_2858.jpg
-#feh --randomize --bg-fill ~/Képek/*
-#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
-
-
 #emojione-picker
 redshift -x
 redshift -O 4000
 #ckb-next
-#fetchcord --nohardware
 
 $HOME/.config/polybar/launch.sh &
-
+dunst &
+run nitrogen --restore
