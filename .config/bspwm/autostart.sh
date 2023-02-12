f92c2ed9d95cae7d7eb02657f2c46c63cd285bca
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-12 11:37:46 -0800

    updated bspwm scripts

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -13,8 +13,6 @@ function run {
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
-killall conky
-conky -c $HOME/.config/bspwm/system-overview &
 run nm-applet &
 run pamac-tray &
 numlockx on &
@@ -24,6 +22,7 @@ picom --experimental-backends --config $HOME/.config/picom/picom.conf &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
 
+Hyprland
 #emojione-picker
 redshift -x
 redshift -O 4000
@@ -31,3 +30,6 @@ redshift -O 4000
 dunst &
 emacsclient -e '(kill-emacs)'
 /usr/bin/emacs --daemon
+
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-0 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 3000x420 --rotate normal
+
